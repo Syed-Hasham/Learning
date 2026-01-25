feb3ebc5b8432d6f48d624472dccbcf286d196ff
Syed Hasham <syedhasham420666@gmail.com>
2026-01-26 01:41:14 +0500

    Add files via upload

---
+++ Python/CH#6_Conditional_Expressions.py
@@ -0,0 +1,10 @@
+print("Conditional Expressions")
+a = int(input("Enter Your Age: "))
+if(a>=18):
+    print("You are an adult!")
+elif(a<=0):
+    print("Invalid Age!")
+else:
+    print("You are not an adult!")
+
+print("If Elif Else Ladder")
