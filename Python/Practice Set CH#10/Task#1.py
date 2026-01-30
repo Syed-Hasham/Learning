3f1afc9b01d1a2481a2d2ce8b496fc5d27566d16
Syed Hasham <syedhasham420666@gmail.com>
2026-01-30 18:10:03 +0500

    Create Task#1.py

---
+++ Python/Practice Set CH#10/Task#1.py
@@ -0,0 +1,9 @@
+class programmer:
+    Company="Microsoft"
+    def __init__(self,name,salary,pin):
+        self.name=name
+        self.salary=salary
+        self.pin=pin
+
+p1=programmer("XYZ",12321231,2123)
+print(p1.Company," ",p1.name," ",p1.salary)
