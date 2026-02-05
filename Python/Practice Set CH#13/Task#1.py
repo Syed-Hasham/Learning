a87b233a2380e35323f5c4a8eba5bd9f7124ebaa
Syed Hasham <syedhasham420666@gmail.com>
2026-02-05 23:09:17 +0500

    Create Task#1.py

---
+++ Python/Practice Set CH#13/Task#1.py
@@ -0,0 +1,7 @@
+#python -m venv env1
+#env1\Scripts\activate
+#First Environment : pip install numpy pandas requests
+#pip freeze > requirements.txt
+#python -m venv env2
+#env2\Scripts\activate
+#pip install -r requirements.txt
